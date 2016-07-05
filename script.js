6e96e169e46e55e89e66c4ccb9f6168a85ef2421
dezolkin@gmail.com <dezolkin@gmail.com>
2016-07-05 19:02:44 +0300

    Refactored version after the first training

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// data является в данном случае нашим источником данных и имеет 3 метода
 
 var data = (function() {
 
@@ -80,201 +81,126 @@ var data = (function() {
 
 })();
 
+
+// Начало нашего приложения. Было принято решение отрефакторить код, чтобы не загромождать наш главный компонент HierarchyComponent функционалом, отвечающим за вызов  
+
+
+// Обратите внимание - компоненты, которые не имеют внутреннего состояния, а только отображают данные, которые приходят из родительского компонета или вызывают метода родителя представлены обычными функциями, которые принимают объект props в качестве аргумента и возвращают JSX код
+
+
+// Элемент списка иерархии
+var NodeItem = function(props) {
+
+    // логика отображения теперь полностью реализуется внутри этого компонента
+    var style = { width: (props.node.hasChildren ? '90%' : '100%') };
+
+    // (1) В качестве props.children будет выступать содержимое, передаваемое в родительском компоненте -> (2)
+    return (
+
+        <li className="item">
+            <div className="text" style={ style }>{ props.node.title }</div>
+            { props.children }
+        </li>
+
+    );
+}
+
+
+var DrillDown = function(props) {
+
+    // (3) Который в свою очередь возращает либо разметку кнопки с обработчиком, либо null. Все эти манипуляции позволяют нам максимально упростить код главного компонента -> (4) в самом низу
+    return (
+
+        props.show
+            ? <div className="expand" onClick={ function(e) { props.drilldown(props.toLevel) } }></div>
+            : null
+
+    );
+}
+
+var NodeList = function(props) {
+
+    return (
+
+        <ul className="list">
+            {
+                props.items.
+                map(function(item) {
+
+                    // (2) В качестве props.children выступает элемент DrillDown -> (3)
+                    return (
+                        <NodeItem node={ item }>
+                            <DrillDown drilldown={ props.setLevel } toLevel={ item.title } show={ item.hasChildren }/>
+                        </NodeItem>
+                    );
+                })
+            }
+        </ul>
+
+    );
+};
+
 var HierarchyComponent = React.createClass({
 
+
     getInitialState: function() {
+
         return {
-            collapsed: true,
-            text: 'National'
+            toggle: false,
+            parentFilter: 'Top'
         }
     },
 
-    changeCollapsed: function() {
-        this.setState({ collapsed: !this.state.collapsed, text: this.state.text });
+    toggle: function() {
+        this.setState({ toggle: !this.state.toggle });
+        console.log(this.state.toggle);
     },
 
-    setText: function(txt) {
-        this.setState({ collapsed: true, text: txt });
-    },
+    getItems: function() {
 
-    render: function() {
+        var self = this;
 
-        var toggleClass = this.state.collapsed ? 'toggle' : 'toggle--down';
-
-        return  (
-            <div className="wrapper">
-                <div className="dropdown" onClick={ this.changeCollapsed }>
-                    <div className={ toggleClass }></div>
-                    <div className="text">{ this.state.text }</div>
-                </div>
-                <HierarchyList show={ !this.state.collapsed } hierarchy={ data.getItems() } initialNode={ this.state.text } levelHandler={ this.setText } />
-            </div>
-        );
-    }
-});
-
-var HierarchyList = React.createClass({
-
-    getInitialState: function() {
-        return {
-            filterType: 'filterByParent',
-            selectedNode: this.props.initialNode,
-            pendingNode: null,
-            parentFilter: this.props.initialNode,
-            inputFilter: ''
-        }
-    },
-
-    componentWillReceiveProps(nextProps) {
-        this.setState({
-            selectedNode: nextProps.initialNode,
-            pendingNode: null,
-            inputFilter: '',
-            filterType: 'filterByParent',
-            parentFilter: (nextProps.initialNode == 'National' ? 'National' : data.getParentName(nextProps.initialNode))
+        return data.getItems().
+        filter(function(item, id) {
+            return item.parent === self.state.parentFilter;
         });
     },
 
-    getItemCollection: function() {
-
-        var self = this;
-
-        return self.props.hierarchy.
-            filter(function(item) {
-                if (self.state.filterType == 'filterByParent') {
-                    return item.parent === self.state.parentFilter;
-                } else {
-                    return item.title.toLowerCase().indexOf(self.state.inputFilter.toLowerCase()) !== -1;
-                }
-            });
-    },
-
-    setSelectedNode: function(node) {
-        this.setState({ pendingNode: node });
-    },
-
-    setFilterByParent: function(newParent) {
-        this.setState({ filterType: 'filterByParent',  parentFilter: newParent, inputFilter: '' });
-    },
-
-    setSearchValue: function(val) {
-        this.setState({ filterType: 'filterByInput', inputFilter: val });
+    setParentFilter: function(parent) {
+        this.setState({ parentFilter: parent });
     },
 
     render: function() {
 
-        var self = this;
+        var style = { display: (!this.state.toggle ? 'none' : 'initial') };
 
-        var style = {
-            display: (this.props.show ? 'initial' : 'none')
-        };
+        var className = (!this.state.toggle ? 'toggle' : 'toggle--down');
 
-        return (
-            <div className="body" style={ style }>
-                <div className="header">
-                    <SearchFiled value={ self.state.inputFilter } changeHandler={ self.setSearchValue } />
-                    <BreadCrumb selected={ self.state.parentFilter } clickCrumb={ self.setFilterByParent } />
+
+        // (4) Всю логику построения разметки и работы с состоянием мы переложили на компонент NodeList и его вложенные компоненты
+        return  (
+            <div className="wrapper">
+                <div className="dropdown" onClick={ this.toggle } >
+                    <div className={ className }></div>
+                    <div className="text">National</div>
                 </div>
-                <ul className="list">
-                    { self.getItemCollection().
-                        map(function(item, id) {
-
-                            item.isSelected = (item.title === self.state.pendingNode || (!self.state.pendingNode && item.title === self.state.selectedNode));
-
-                            return(
-                                <HierarchyNode key={id} selectNode={ self.setSelectedNode } drilldown={ self.setFilterByParent } node={ item } />
-                            )
-                        })
-                    }
-                </ul>
-                <div className="footer">
-                    <a className="cancel" onClick={ function(e) { e.preventDefault(); self.props.levelHandler(self.state.selectedNode) } }>Cancel</a>
-                    <button className="select" onClick={ function(e) { e.preventDefault(); self.props.levelHandler((self.state.pendingNode ? self.state.pendingNode : self.state.selectedNode)) } }>Apply</button>
+                <div style={ style } className="body">
+                    <div className="header">
+                        <input type="text" className="search-input" placeholder="Search" />
+                        <div className="breadcrumb">
+                            <span>Top</span><span className="selected">National</span>
+                        </div>
+                    </div>
+                    <NodeList items={ this.getItems() } setLevel={ this.setParentFilter } />
+                    <div className="footer">
+                        <a className="cancel">Cancel</a>
+                        <button className="select">Apply</button>
+                    </div>
                 </div>
             </div>
         );
     }
 });
-
-function SearchFiled(props) {
-
-    function handleChange(e) {
-        props.changeHandler(e.target.value);
-    }
-
-    return (
-        <input
-            className="search-input"
-            placeholder="Search"
-            type="text"
-            value={props.value}
-            onChange={handleChange}
-        />
-    );
-}
-
-function BreadCrumb(props) {
-
-    function buildCrumbs() {
-
-        return data.buildBreadcrumb(props.selected).
-            map(function(item, idx) {
-
-                var isCurrentLevel = props.selected === item;
-
-                var className = (isCurrentLevel ? 'selected' : '');
-
-                var clickHandler = ( isCurrentLevel ? null : function() { props.clickCrumb(item) } );
-
-                return <span key={idx} className={ className } onClick={ clickHandler }>{ item }</span>;
-            });
-    }
-
-    return (
-        <div className="breadcrumb">
-            { buildCrumbs() }
-        </div>
-    );
-
-}
-
-function HierarchyNode(props) {
-
-    function onClick(e) {
-        e.preventDefault();
-        props.selectNode(props.node.title);
-    }
-
-    var style = {
-        width: (props.node.hasChildren ? 90 : 100) + '%'
-    };
-
-    var className = "text" + (props.node.isSelected ? ' active' : '');
-
-    var DrillDownButton = null;
-    if (props.node.hasChildren) DrillDownButton = <NodeFilterButton clickHandler={ props.drilldown } newParent={ props.node.title } />;
-
-    return (
-        <li className="item">
-            <div className={ className } style={ style } onClick={ onClick }>
-                { props.node.title }
-            </div>
-            { DrillDownButton }
-        </li>
-    );
-}
-
-function NodeFilterButton(props) {
-
-    function onClick(e) {
-        e.preventDefault();
-        props.clickHandler(props.newParent);
-    }
-
-    return (
-        <div className="expand" onClick={ onClick }></div>
-    );
-}
 
 ReactDOM.render(
     <HierarchyComponent />,
